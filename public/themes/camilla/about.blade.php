fc91b2aa19cc9e2c66ce9fd1430f3b7cad1bdb4f
CamillaGejl <camillagejlolsen@gmail.com>
2014-11-20 18:09:35 +0100

    Update about.blade.php

--- public/themes/camilla/about.blade.php
+++ public/themes/camilla/about.blade.php
@@ -11,14 +11,18 @@ about
 @section('content')
 	<section class="about">
 		<p>
-			My name is Camilla Gejl, I'm 18 years old and live in Randers, Denmark. I'm currently studying Social Science at Langkaer Gymnasium in Aarhus, wanting to study Sociology when I've graduated.
+			Keep updated on my blog:<br>
+			Facebook: <a href="https://www.facebook.com/pages/CamillaGejlcom/1474320836119987?fref=ts" target="_blank" title="Visit my Facebook-page">CamillaGejl.com</a><br>
+			Twitter: <a href="https://twitter.com/CamillaGejl_com" target="_blank" title="Visit my Twitter-profile">@CamillaGejl_com</a>
 		</p>
 		<p>
-			I've made this blog, because I always have too many opinions about things to keep spamming facebook with it. ;) In general, I think my facebook friends might be quite annoyed by my face being on their frontpage all the time, saying "Camilla commented on -insert newspage-'s status", and stuff like that, so I'll try to put my thoughts on this page instead.
+			My name is Camilla Gejl, I'm 18 years old and live in Randers, Denmark. I'm currently studying Social Science at Langkaer Gymnasium in Aarhus, wanting to study Religious Studies when I've graduated.
 		</p>
 		<p>
-			I will probably mainly post about politics and other social matters, but since I live in Denmark, it'd be quite inappropriate to comment on my country's politics in English. Therefore, you might see a lot of Danish posts coming as well - but I'll keep as much in English as I can.
-			It won't be all about politics though - this blog will be about everything that comes to my mind; politics, movies, anime/cosplay, photography or whatever else of my interests. :) So I hope I will post something that will interest you. :D
+			I've made this blog, because I always have too many opinions about things to only spam facebook with it. Though I often comment on articles I find on facebook, I like to have some more space to do with - and I hope you like to read it!
+		</p>
+		<p>
+			I mainly post about politics and other social matters, but since I live in Denmark, it'd be quite inappropriate to comment on my country's politics in English. Therefore, you might see some Danish posts coming as well - but I'll keep as much in English as I can. It is not all about politics though - this blog will be about everything that comes to my mind; politics, movies, anime/cosplay, photography or whatever else of my interests. So I hope I will post something that sparks your interest.
 		</p>
 		<p>
 			I hope you enjoy your stay~
